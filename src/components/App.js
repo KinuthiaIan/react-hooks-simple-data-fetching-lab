dc2855244195d76e8f1a67bf3dbcb7f96c32b5ca
Ian Kinuthia 254 <144425644+KinuthiaIan@users.noreply.github.com>
2024-02-05 15:20:35 +0300

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,28 @@
 // create your App component here
+
+import { useEffect } from "react"
+import React  from "react"
+import { useState } from "react"
+
+function App() {
+    const[dogImage, setDogImage] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false);
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((response) => response.json())
+        .then((data) => {
+           setDogImage(data.message);
+           setIsLoaded(true);
+        });
+    }, [])   //Empty dependencies array to run the fetch request once
+//Showing a loading indicator if data hasn't been loaded.
+    if(!isLoaded) 
+    return <p>Loading...</p>
+  return (
+    <div>
+   <img src={dogImage} alt="A Random Dog"/> {/*Displaying the image */}
+    </div>
+  )
+}
+
+export default App
